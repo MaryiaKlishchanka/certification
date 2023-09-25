22d9c078a8040c43c81305d21099042e2e052381
Maryia <asha.87@list.ru>
2023-09-25 23:26:47 +0300

    добавила 66 test

---
+++ test66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу,
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);  
+}
+
+int SumNumbers(int m, int n)
+{
+    
+    if (m>n) return 0;
+    else if (m==n) return n;
+    else return n + SumNumbers(m, --n);
+}
+
+
+int start = InputNum("введите число M: ");
+int end = InputNum("введите число N: ");
+int sum = SumNumbers(start, end);
+Console.WriteLine(sum);
+
